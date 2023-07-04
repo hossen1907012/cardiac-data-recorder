b2076f2ccde05b953497b119a18d9a31297688fc
unknown <chinmoymodak5500@gmail.com>
2023-07-04 19:32:04 +0600

    App Icon and User Authentication added

--- app/src/main/java/com/example/cardiac_data_recoder/DetailsActivity.java
+++ app/src/main/java/com/example/cardiac_data_recoder/DetailsActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class OtherActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_other);
+        setContentView(R.layout.activity_details);
     }
 }
