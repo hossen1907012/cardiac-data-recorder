7c9a21a71164ae249415568cc6a9dea7081b0fa4
unknown <chinmoymodak5500@gmail.com>
2023-07-04 15:11:12 +0600

    splashscreen added

---
+++ app/src/main/java/com/example/cardiac_data_recoder/OtherActivity.java
@@ -0,0 +1,14 @@
+package com.example.cardiac_data_recoder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
