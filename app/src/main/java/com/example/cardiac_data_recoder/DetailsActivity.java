d5d0688bfdeb1bfbda3357bb908c47168f9ef6c2
unknown <chinmoymodak5500@gmail.com>
2023-07-05 17:51:52 +0600

    mine details added

--- app/src/main/java/com/example/cardiac_data_recoder/DetailsActivity.java
+++ app/src/main/java/com/example/cardiac_data_recoder/DetailsActivity.java
@@ -2,13 +2,23 @@ package com.example.cardiac_data_recoder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class DetailsActivity extends AppCompatActivity {
+
+    FloatingActionButton addNoteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        addNoteBtn=findViewById(R.id.add_note_btn);
+
+
+
     }
 }
