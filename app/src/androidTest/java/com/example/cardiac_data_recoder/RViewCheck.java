9ee54fb67045bc3730153822411e892be98afc50
unknown <chinmoymodak5500@gmail.com>
2023-07-06 01:43:51 +0600

    UI Test Added

--- app/src/androidTest/java/com/example/cardiac_data_recoder/RViewCheck.java
+++ app/src/androidTest/java/com/example/cardiac_data_recoder/RViewCheck.java
@@ -1,4 +1,63 @@
+package com.example.cardiac_data_recoder;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+
 public class RViewCheck {
-  
+
+    @Rule
+    public ActivityScenarioRule<DataViewerActivity> activityRule = new ActivityScenarioRule<>(DataViewerActivity.class);
+
+    @Test
+    public void UiTest() {
+
+
+        onView(withId(R.id.redirect_to_add_data)).perform(click());
+
+        try {
+            Thread.sleep(1500); // Adjust the delay as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            Thread.sleep(1500); // Adjust the delay as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.insert_medical_report_layout)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.systolic_pressure)).perform(ViewActions.typeText("100"));
+        onView(withId(R.id.diastolic_pressure)).perform(ViewActions.typeText("100"));
+        onView(withId(R.id.heart_rate)).perform(ViewActions.typeText("100"));
+        onView(withId(R.id.comment)).perform(ViewActions.typeText("I'm Perfect!"));
+
+        Espresso.pressBack();
+        onView(withId(R.id.add_new_record)).perform(click());
+
+        try {
+            Thread.sleep(500); // Adjust the delay as needed
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.cardiac_report_view)).check(matches(isDisplayed()));
+
+
+    }
 }
--- app/src/androidTest/java/com/example/cardiac_data_recoder/UiTest.java
+++ app/src/androidTest/java/com/example/cardiac_data_recoder/UiTest.java
@@ -7,24 +7,23 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
+@RunWith(AndroidJUnit4.class)
 
 public class UiTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
-    public ActivityScenarioRule<com.example.cardiac_data_recoder.auth.loginactivity1> loginactivityrule = new ActivityScenarioRule<>(com.example.cardiac_data_recoder.auth.loginactivity1.class);
-
-
 
 
 
@@ -37,34 +36,37 @@ public class UiTest {
         onView(withId(R.id.splashScreenImg)).check(matches(isDisplayed()));
         onView(withId(R.id.splashScreenImg)).check(matches(isDisplayed()));
 
-    }
-
-    @Test
-    public void testlogin()
-    {
         try {
             Thread.sleep(3500); // Adjust the delay as needed
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        onView(withId(R.id.loginscreen)).check(matches(isDisplayed()));
-
-        onView(withId(R.id.signupemail1)).perform(ViewActions.typeText("turjo.chinmoymodak01@gmail.com"));
-        onView(withId(R.id.signuppassword2)).perform(ViewActions.typeText("1234Turjo@"));
-        onView(withId(R.id.login1)).perform(click());
-
-
-        try {
-            Thread.sleep(4000); // Adjust the delay as needed
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        onView(withId(R.id.redirect_to_add_data)).check(matches(isDisplayed()));
-
-
     }
+
+
+
+//    @Test
+//    public void testadd()
+//    {
+//        try {
+//            Thread.sleep(1500); // Adjust the delay as needed
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        onView(withId(R.id.redirect_to_add_data)).perform(click());
+//        onView(withId(R.id.insert_medical_report_layout)).check(matches(isDisplayed()));
+//        onView(withId(R.id.systolic_pressure)).perform(ViewActions.typeText("120"));
+//        onView(withId(R.id.diastolic_pressure)).perform(ViewActions.typeText("80"));
+//        onView(withId(R.id.heart_rate)).perform(ViewActions.typeText("70"));
+//        onView(withId(R.id.comment)).perform(ViewActions.typeText("I'm Perfect!"));
+//
+//        // Open the DatePicker
+//        //onView(withId(R.id.datePickerButton)).perform(click())
+//        onView(withId(R.id.add_new_record)).perform(click());
+//
+//    }
 
 
 
